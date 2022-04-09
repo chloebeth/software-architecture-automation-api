ec4252854d698a867732284ab7e88d811d4a9963
Chloe Muncy <chloe.e.muncy@gmail.com>
2022-04-08 20:45:34 -0400

    Update AutomationAPI_PartBuilder.h

--- AutomationBinding/AutomationAPI_PartBuilder.h
+++ AutomationBinding/AutomationAPI_PartBuilder.h
@@ -2,26 +2,45 @@
 #include "AutomationBindingExports.h"
 #include "AutomationAPI_IBuilder.h"
 
+/** <summary>
+PartBuilder API creates and manages parts associated with specific bindings.
+</summary>*/
+
+/** <remarks>
+There is nothing to explain here as this API consists of scaffolding and empty returns. 
+</remarks>*/
+
 namespace AutomationAPI
 {
     class Part;
     class PartBuilder;
 
+    /*! \fn class Part
+     * Class Part.
+     */
     class AUTOMATIONBINDING_API PartBuilder : public IBuilder
     {
     public:
+        /*! PartBuilder details. */
         PartBuilder();
+        
+        /*! PartBuilder destructor with details. */
         virtual ~PartBuilder();
 
+        /*! The different types that PartBuilder can be created as. Plus, more details. */
         enum PartBuilderTypes
         {
             TypeDiagonalPart,
             TypeSquarePart
         };
 
+        /*! Returns the length of the PartBuilder. */
         void GetLength();
+        
+        /*! Returns the ID of PartBuilder. */
         void GetID();
 
+        /*! Creates a new part and returns it to be used elsewhere. Plus, more details. */
         Part* CreatePart();
     };
 
